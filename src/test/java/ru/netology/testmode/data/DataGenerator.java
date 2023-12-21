363a77065df2a52836d0c958f4e203777fa5f6af
Locapeople <missis.anyack@yandex.ru>
2023-12-21 11:50:25 +0300

    fix generator: remove excessives

--- src/test/java/ru/netology/testmode/data/DataGenerator.java
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -66,23 +66,5 @@ public class DataGenerator {
         String login;
         String password;
         String status;
-
-        public RegistrationDto(String login, String password, String status) {
-            this.login = login;
-            this.password = password;
-            this.status = status;
-        }
-
-        public String getLogin() {
-            return login;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public String getStatus() {
-            return status;
-        }
     }
 }
